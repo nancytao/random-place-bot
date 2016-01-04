de63abfe4926a66a9511fc77d4c5a90c62634f4e
nancytao <nancy.tao42@gmail.com>
2016-01-04 15:39:44 -0600

    Made a random word function

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import requests
 
-random_word = requests.get('http://randomword.setgetgo.com/get.php')
-random_word = random_word.text.replace('\n', '').replace('\r', '')
+def random_word():
+    word = requests.get('http://randomword.setgetgo.com/get.php')
+    return word.text.replace('\n', '').replace('\r', '')
 
-print(random_word)
+print(random_word())
