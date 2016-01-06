20e7331320bd73da009bb4fbb1a377b527238759
nancytao <nancy.tao42@gmail.com>
2016-01-05 19:11:56 -0600

    Made random word of set length and added stuff to connect to twitter

--- bot.py
+++ bot.py
@@ -1,9 +1,17 @@
-import requests, config
+import requests
+import random
+import config, urllib, urllib2, json, tweepy
 
 
 def random_word():
-    wlength = {'len': '6'}
+    wlength = {'len': random.randrange(3, 19)}
     return requests.get('http://randomword.setgetgo.com/get.php', params=wlength).text.strip()
+
+CONSUMER_KEY = 'vNXrQnSpIDQecNmt2GWtotrxb'
+ACCESS_KEY = '4704978270-fKtYR3DjbVy5agimR6vrUkaw7msmbMFqMCzVDsT'
+auth = tweepy.OAuthHandler(CONSUMER_KEY, config.TWITTER_CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, config.TWITTER_ACCESS_SECRET)
+api = tweepy.API(auth)
 
 three_words = [random_word(), random_word(), random_word()]
 print(three_words)
