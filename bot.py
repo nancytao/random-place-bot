0f69efb780c98a516adf17de2782425c10f72ad8
nancytao <nancy.tao42@gmail.com>
2016-01-07 20:58:39 -0600

    Now generates random words until it finds a corresponding location

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ import tweepy
 import urllib
 
 
-def getPosition(words="clashes.crib.purses"):  # default is Georgia Tech college of computing
+def get_position(words="clashes.crib.purses"):  # default is Georgia Tech college of computing
     if isinstance(words, list):
         words = "%s.%s.%s" % (words[0], words[1], words[2])
 
@@ -20,15 +20,27 @@ def getPosition(words="clashes.crib.purses"):  # default is Georgia Tech college
 
 
 def random_word():
-    wlength = {'len': random.randrange(3, 19)}
+    wlength = {'len': 5}
     return requests.get('http://randomword.setgetgo.com/get.php', params=wlength).text.strip()
 
-CONSUMER_KEY = 'vNXrQnSpIDQecNmt2GWtotrxb'
-ACCESS_KEY = '4704978270-fKtYR3DjbVy5agimR6vrUkaw7msmbMFqMCzVDsT'
-auth = tweepy.OAuthHandler(CONSUMER_KEY, config.TWITTER_CONSUMER_SECRET)
-auth.set_access_token(ACCESS_KEY, config.TWITTER_ACCESS_SECRET)
-api = tweepy.API(auth)
+# CONSUMER_KEY = 'vNXrQnSpIDQecNmt2GWtotrxb'
+# ACCESS_KEY = '4704978270-fKtYR3DjbVy5agimR6vrUkaw7msmbMFqMCzVDsT'
+# auth = tweepy.OAuthHandler(CONSUMER_KEY, config.TWITTER_CONSUMER_SECRET)
+# auth.set_access_token(ACCESS_KEY, config.TWITTER_ACCESS_SECRET)
+# api = tweepy.API(auth)
 
 three_words = [random_word(), random_word(), random_word()]
 
-print getPosition()
+print three_words
+position = get_position(three_words)
+
+while True:
+    try:
+        throw_away = position['error']
+        three_words = [random_word(), random_word(), random_word()]
+        print three_words
+        position = get_position(three_words)
+    except KeyError:
+        break
+
+print get_position(three_words)
